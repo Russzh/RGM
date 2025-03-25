0f1f4773681f47273638420080ee3ee9ca607866
Russzh <russ.zhukovsky@gmail.com>
2025-03-25 14:11:43 +0300

    Task-2: Testing: jest and coverage adjustment

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.{js,jsx,ts,tsx}",
+    "!src/**/*.d.ts",
+    "!src/index.tsx",
+    "!src/reportWebVitals.ts",
+  ],
+  coverageReporters: ["text", "lcov", "json", "html"],
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
+  },
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
+  moduleNameMapper: {
+    "\\.(css|scss)$": "identity-obj-proxy",
+  },
+};
